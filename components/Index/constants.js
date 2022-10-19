6df9e29077ebdcfa38021e49785b7b97a705fe8e
Mohit Tyagi <mohit@hey.com>
2022-10-19 23:53:03 +0530

    feat: project init

---
+++ components/Index/constants.js
@@ -0,0 +1,77 @@
+export const DATA = [{
+    
+    name: "Financial Services",
+    weightage: 36.82,
+    stocks: [{
+
+        name: "HDFC Bank Ltd.",
+        weightage: 8.27
+
+    }]
+
+},{
+    
+    name: "Information Technology",
+    weightage: 13.82,
+
+},{
+    
+    name: "Oil, Gas & Consumable Fuels ",
+    weightage: 12.47,
+
+},{
+    
+    name: "Fast Moving Consumer Goods",
+    weightage: 9.18,
+
+},{
+    
+    name: "Automobile and Auto Components",
+    weightage: 5.83,
+
+},{
+    
+    name: "Healthcare",
+    weightage: 4.06,
+
+},{
+    
+    name: "Metals & Mining",
+    weightage: 3.85,
+
+},{
+    
+    name: "Consumer Durables",
+    weightage: 3.43,
+
+},{
+    
+    name: "Construction",
+    weightage: 2.95,
+
+},{
+    
+    name: "Telecommunication",
+    weightage: 2.59,
+
+},{
+    
+    name: "Power",
+    weightage: 1.96,
+
+},{
+    
+    name: "Construction Materials",
+    weightage: 1.79,
+
+},{
+    
+    name: "Services",
+    weightage: 0.78,
+
+},{
+    
+    name: "Chemicals",
+    weightage: 0.47,
+
+}]

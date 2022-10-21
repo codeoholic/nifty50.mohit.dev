f3d44720ec31bef8a1f38c93befa87ce84923731
Mohit Tyagi <mohit@hey.com>
2022-10-21 11:57:26 +0530

    feat: adding fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
 import '../styles/globals.css'
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import * as Fathom from "fathom-client"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  	const router = useRouter()
+	useEffect(() => {
+
+		Fathom.load("XZYPYEXT",{
+
+			excludedDomains:["localhost"],
+			url:"https://inventive-elastic.mohit.dev/script.js",
+
+		})
+
+		const onRouteChangeComplete = () => {
+
+			Fathom.trackPageview()
+
+		}
+		// RECORD A PAGEVIEW WHEN ROUTE CHANGES
+		router.events.on("routeChangeComplete", onRouteChangeComplete)
+
+		// UNASSIGN EVENT LISTENER
+		return () => {
+
+			router.events.off("routeChangeComplete", onRouteChangeComplete)
+
+		}
+
+	}, [])
+
+  	return <Component {...pageProps} />
+
 }
 
 export default MyApp
